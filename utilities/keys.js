4cbb7b83f31874eeb70200a3179cfe1807cf8a16
Logan Hasty <loganhasty@gmail.com>
2017-11-09 17:35:26 -0500

    Add files via upload

---
+++ utilities/keys.js
@@ -0,0 +1,11 @@
+exports.fitBits = {
+    CLIENT_ID: '22CKTF',
+    CLIENT_SECRET: 'd645df21ea014a51750631a2ae3f0347',
+    user_id: '3BLB34',
+    bearerToken: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIzQkxCMzQiLCJhdWQiOiIyMkNLVEYiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJybG9jIHJhY3QgcmhyIHJwcm8iLCJleHAiOjE1MTAyOTIxODksImlhdCI6MTUxMDI2MzM4OX0.JrXqP1niaeMok9z-IMlQH_UnXaZZOiVgvZEHw6IYrSA'
+  };
+  
+  exports.sugarBits = {
+    token: '14784-aeb0a9a6fd2d05f9213c5c49f3c74c6e',
+    xMashapeKey: '6jSePifxX0mshxd2XWBWPbHlepblp1Xtffhjsnsd7sZZyeYkRa'
+  };
