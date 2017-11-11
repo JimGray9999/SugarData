b13d7cd9b5adf2bc073726aff5af4ddfe0f0e455
Logan Hasty <loganhasty@gmail.com>
2017-11-11 08:00:58 -0500

    Delete App.test.js

--- App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import StackNav from './StackNavigator';
-
-import renderer from 'react-test-renderer';
-
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
